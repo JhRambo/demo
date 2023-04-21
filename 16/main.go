122691ec38e6f056fea180b1eb73f370810eac1c
zqw <237230999@qq.com>
2023-04-21 17:43:17 +0800

    demo

--- 16/main.go
+++ 16/main.go
@@ -11,17 +11,17 @@ var wg sync.WaitGroup
 var mutex sync.RWMutex //读写互斥锁
 
 func fn1() {
-	mutex.Lock()
+	mutex.Lock() //写锁	独占
 	fmt.Println("写数据")
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 2) //每个goroutine休眠时间
 	mutex.Unlock()
 	wg.Done()
 }
 
 func fn2() {
-	mutex.RLock()
+	mutex.RLock() //读锁	共享
 	fmt.Println("....读数据")
-	time.Sleep(time.Second * 1)
+	time.Sleep(time.Second * 1) //每个goroutine休眠时间
 	mutex.RUnlock()
 	wg.Done()
 }
---
+++ 76/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func g1() {
+	log.Println("========g1")
+}
+func g2() {
+	log.Println("g2========")
+}
+
+func main() {
+	go g1()
+	go g2()
+	log.Println("========================================main")
+	time.Sleep(time.Millisecond * 1)
+}
