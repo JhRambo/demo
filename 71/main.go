b2d2ef6dece38da77f3cca3c80caeeefdd4b0151
zqw <237230999@qq.com>
2023-04-24 10:12:31 +0800

    demo

--- 71/main.go
+++ 71/main.go
@@ -9,15 +9,14 @@ import (
 func Monitor(ctx context.Context) {
 	select {
 	case <-ctx.Done():
-		fmt.Println(ctx.Err())
-	case <-time.After(5 * time.Second):
+		fmt.Println(ctx.Err()) //context deadline exceeded
+	case <-time.After(2 * time.Second):
 		fmt.Println("stop monitor")
 	}
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
-	go Monitor(ctx)
-	time.Sleep(5 * time.Second)
+	Monitor(ctx)
 }
