734c49dacca6bd190e34b4b7c6a9aa8832668d53
zqw <237230999@qq.com>
2023-06-25 16:37:08 +0800

    stash

--- 98/main.go
+++ 98/main.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	var nodes interface{}
 	nodes = "123456"
+	nodes = 123456
 
 	v := reflect.ValueOf(nodes)
 
+	fmt.Printf("%T,%s\n", v, v)
+	fmt.Println("reflect...")
 	fmt.Printf("%T,%s\n", v, v)
 }
