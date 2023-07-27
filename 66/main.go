76d3128dc4e26b475f673ba47838dd5d37397ba0
zqw <237230999@qq.com>
2023-07-27 09:43:13 +0800

    test

--- 66/main.go
+++ 66/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	//自定义error内容
 	e := errors.New("SigningMethodError")
 	fmt.Println(e) //SigningMethodErro
 }
--- 67/main.go
+++ 67/main.go
@@ -7,4 +7,5 @@ import (
 
 func main() {
 	fmt.Println(time.Duration(125 * time.Second)) //2m5s
+	fmt.Println(125 * time.Second)                //2m5s
 }
