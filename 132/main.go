543f593c4fedf80db20961feee213ea30b256d8c
zqw <237230999@qq.com>
2023-09-18 16:53:37 +0800

    demo

--- 132/main.go
+++ 132/main.go
@@ -9,10 +9,16 @@ import (
 func main() {
 	router := gin.Default()
 
-	// 定义路由和处理函数
+	// 定义路由和处理函数 uri path 的区别
 	router.GET("/hello", func(c *gin.Context) {
 		fmt.Println(c.Request.RequestURI) // /hello?a=1&b=2
 		fmt.Println(c.Request.URL.Path)   // /hello
+	})
+
+	// 定义路由和处理函数 uri path 的区别
+	router.POST("/golang", func(c *gin.Context) {
+		fmt.Println(c.Request.RequestURI) // /golang?a=1&b=2
+		fmt.Println(c.Request.URL.Path)   // /golang
 	})
 
 	// 启动 Gin 服务器
